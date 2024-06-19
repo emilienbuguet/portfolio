daed1579955ea7d22f3f0a3439867a26217e26c1
emilienbuguet <emilien.buguet@epitech.eu>
2024-06-19 09:41:53 +0200

    fix: experience cards

--- src/molecules/ExperienceCard.tsx
+++ src/molecules/ExperienceCard.tsx
@@ -12,34 +12,21 @@ export function ExperienceCard({name}: ExperienceCardProps) {
     }, [navigate, name]);
 
     return (
-        <div className='border-2 hover:border-primary rounded-xl w-full h-48 md:h-96 flex flex-col cursor-pointer' onClick={onClick}>
-            <div className='h-1/6 bg-primary rounded-t-lg flex items-center justify-center text-white font-abhaya font-extrabold text-xl md:text-3xl'>
-                {t(`experience_company_name_${name}`)}
+        <div className='border-2 hover:border-primary rounded-xl w-full h-48 md:h-96 flex flex-col cursor-pointer relative' onClick={onClick}>
+            <div className='h-1/3 border-b-2 rounded-t-lg'>
+                <img className='w-full h-full object-cover rounded-t-lg'  src={`/portfolio/experiences/${name}_banner.png`}/>
             </div>
-            <div className='flex flex-col p-4 h-full items-center gap-4 justify-center'>
-                <div className='flex-1 flex justify-center items-center'>
-                    <div className='text-xl md:text-2xl lg:text-3xl font-abhaya font-extrabold text-center text-gray-600'>
-                        {t(`experience_title_${name}`)}
-                    </div>
-                </div>
-                <div className='flex-1'>
-                    <div className="h-6 w-6 md:h-12 md:w-12 flex items-center justify-center rounded-full bg-primary">
-                        <FontAwesomeIcon icon={faArrowRight} className='text-white text-md md:text-2xl' />
-                    </div>
-                </div>
+            <div className="w-12 h-12 md:w-24 md:h-24 absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg">
+                <img className='rounded-lg w-full h-full object-cover' src={`/portfolio/experiences/${name}_logo.png`}/>
             </div>
-            {/* <div>
-                <div className='text-xl md:text-2xl lg:text-4xl font-abhaya font-extrabold'>
+            <div className='flex flex-col md:gap-2 p-2 md:p-4 pt-10 md:pt-16 font-abhaya'>
+                <div className='font-extrabold text-md md:text-2xl'>
                     {t(`experience_title_${name}`)}
                 </div>
-                <div className='text-xl md:text-2xl lg:text-4xl font-abhaya font-extrabold'>
+                <div className=' text-sm md:text-xl'>
                     {t(`experience_company_name_${name}`)}
                 </div>
-                <div>
-                    {t(`experience_card_description_${name}`)}
-                </div>
             </div>
-            <div>{t("molecules_experience_card_button")}</div> */}
         </div>
     );
 }
